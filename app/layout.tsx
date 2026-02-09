88aabd8c663744ef741bdb9ef6a7f6b41804b902
Gevorg <narodniyplusmagazin@gmail.com>
2026-02-09 16:44:40 +0400

    add verivication code for google

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="google12bf72eeb4c2f75b.html" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
